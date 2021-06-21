38f71b17ebd81af3a431635f1d780d326b3981ac
Robert Manzke <robert.manzke@fh-kiel.de>
2021-06-21 15:23:59 +0200

    added tbd-sim

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e # stops execution if error on command
 
-echo Starting build tapp win32
+echo Building tapp mingw-win64
 cd tapp
+mkdir build
+cd build
+cmake -DBoost_INCLUDE_DIR=/usr/local/include -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_INSTALL_PREFIX=${MINGW} -DCMAKE_FIND_ROOT_PATH=${MINGW} -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ..
+make
+cpack
+echo Building tbd-sim mingw-win64
+cd ../../simulator
 mkdir build
 cd build
 cmake -DBoost_INCLUDE_DIR=/usr/local/include -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_INSTALL_PREFIX=${MINGW} -DCMAKE_FIND_ROOT_PATH=${MINGW} -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER -DCMAKE_FIND_ROOT_PATH_MODE_INCLUDE=ONLY -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ -DCMAKE_RC_COMPILER=x86_64-w64-mingw32-windres ..
